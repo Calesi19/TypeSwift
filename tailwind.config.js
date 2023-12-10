6799acad4eb86f22af1566edad9084e1221f3100
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-12-09 19:46:27 -0500

    fix: uploaded tailwind files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: [
+    './src/renderer/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+  ],
 
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['Agbalumo', 'sans-serif'],
+        custom: ['Racing', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#FFBCAD',
+          500: '#FE795D',
+          600: '#EF562F',
+          700: '#EB4F27',
+          800: '#CC4522',
+          900: '#A5371B',
+        },
+      },
+    },
+  },
+  plugins: [require('flowbite/plugin')],
+};
